c0734c076c1a2d9beba577de56c5d2861e63a106
luciano-buono <luciano.buono@outlook.com>
2022-02-03 15:09:06 -0300

    Removed one NATGW and edited the Route table. Now there is only one NatGW for all the public subnets

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -25,11 +25,17 @@ module "public_subnet" {
   availability_zones = var.availability_zones
 }
 
+# module "nat" {
+#   source = "../nat_gateway"
+
+#   subnet_ids   = module.public_subnet.ids
+#   subnet_count = length(var.public_subnet_cidrs)
+# }
 module "nat" {
   source = "../nat_gateway"
 
   subnet_ids   = module.public_subnet.ids
-  subnet_count = length(var.public_subnet_cidrs)
+  subnet_count = 1
 }
 
 resource "aws_route" "public_igw_route" {
@@ -39,10 +45,16 @@ resource "aws_route" "public_igw_route" {
   destination_cidr_block = var.destination_cidr_block
 }
 
+# resource "aws_route" "private_nat_route" {
+#   count                  = length(var.private_subnet_cidrs)
+#   route_table_id         = element(module.private_subnet.route_table_ids, count.index)
+#   nat_gateway_id         = element(module.nat.ids, count.index)
+#   destination_cidr_block = var.destination_cidr_block
+# }
 resource "aws_route" "private_nat_route" {
   count                  = length(var.private_subnet_cidrs)
   route_table_id         = element(module.private_subnet.route_table_ids, count.index)
-  nat_gateway_id         = element(module.nat.ids, count.index)
+  nat_gateway_id         = module.nat.ids[0]
   destination_cidr_block = var.destination_cidr_block
 }
 
